25a9defc241d69339eb576f45a8ef6c51c51b3d5
1615Kirito <117769443+1615Kirito@users.noreply.github.com>
2024-12-23 19:53:18 -0500

    Add files via upload

---
+++ ECSEasy_New/src/main/java/mcgill/ECSEasy/EcsEasyApplication.java
@@ -0,0 +1,13 @@
+package mcgill.ECSEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsEasyApplication.class, args);
+	}
+
+}
---
+++ ECSEasy_New/src/test/java/mcgill/ECSEasy/EcsEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package mcgill.ECSEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
